942a0273c83cf4951dbc7f440394a932c97cb2d5
Kamil <krydarow@student.wszi.edu.pl>
2025-12-13 15:51:56 +0100

    Adding 0001_create_users_table.py file

---
+++ app/migrations/versions/0001_create_users_table.py
@@ -0,0 +1,26 @@
+"""create users table
+
+Revision ID: 0001
+Revises: 
+Create Date: 2025-12-06
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = "0001"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), primary_key=True),
+        sa.Column("name", sa.String(length=50), nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table("users")
