7302c3074f4f8a1febb7576b68b741188cb9f48c
Kamil <krydarow@student.wszi.edu.pl>
2025-12-13 16:31:13 +0100

    Changes config.py

--- app/src/config.py
+++ app/src/config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql+psycopg2://postgres:postgres@localhost:5432/devops_db"
+        "postgresql+psycopg2://postgres:postgres@db:5432/devops_db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
