42e248ae4a47d4c9b763cccc97f8c70d5a7f2435
Kamil <krydarow@student.wszi.edu.pl>
2025-12-13 15:36:31 +0100

    Adding routes.py file

---
+++ app/src/routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, jsonify
+from src.models import User
+from src.database import db
+
+bp = Blueprint("api", __name__)
+
+@bp.get("/health")
+def health():
+    return {"status": "ok"}, 200
+
+@bp.get("/users")
+def get_users():
+    users = User.query.all()
+    return jsonify([{"id": u.id, "name": u.name} for u in users])
+
+@bp.post("/users")
+def create_user():
+    user = User(name="Example User")
+    db.session.add(user)
+    db.session.commit()
+    return {"id": user.id, "name": user.name}, 201
